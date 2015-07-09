5d1ae9e2c7bfc4ecf6663c3075f15c19b964e2df
Sean Boulter <sean.boulter@rosetta.com>
2015-07-09 15:03:49 -0400

    Checking in SpecFlow, Selenium Test

---
+++ SeleniumHQ/Steps/SearchSteps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SeleniumHQ.Steps
+{
+    [Binding]
+    public class SearchSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Assert.IsTrue(true);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Assert.IsTrue(true);
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
